10121a354f692db3593043c9b1f9a49bb2f7bfd8
Robson Duarte <robson.o.d@gmail.com>
2020-05-15 12:46:34 -0300

    Pull Up (#26)

---
+++ arduino-courses/arduino-brazilian-course/pull-up/README.md
@@ -0,0 +1,8 @@
+### Pull Up
+
+The simple circuit to Pull Up:
+
+* README IN CONSTRUCTION
+* font about pull up => https://www.instructables.com/id/Understanding-the-Pull-up-Resistor-With-Arduino/
+
+We used the [Clion and Arduino Support](https://github.com/robsonoduarte/learn-arduino/tree/master/clion-arduino/example) to upload the Sketch.
---
+++ arduino-courses/arduino-brazilian-course/pull-up/pull_up.ino
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+int btnPin = 3;
+int ledPin = 10;
+
+void setup() {
+    pinMode(btnPin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    int btnState = digitalRead(btnPin);
+    if(btnState == LOW){
+        digitalWrite(ledPin, HIGH);
+        Serial.println(btnState);
+    } else{
+        digitalWrite(ledPin, LOW);
+        Serial.println(btnState);
+    }
+    delay(1000);
+}
