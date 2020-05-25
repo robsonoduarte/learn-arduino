6c0aceae9ab2643f8e16315d921cccbbcf943e03
Robson Duarte <robson.o.d@gmail.com>
2020-05-25 14:14:43 -0300

    the sketch

--- arduino-courses/arduino-brazilian-course/input-pull-up/input_pull_up.ino
+++ arduino-courses/arduino-brazilian-course/input-pull-up/input_pull_up.ino
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
-void setup() {
+int btnRedPin = 3;
+int btnBluePin = 4;
+int ledRedPin = 10;
+int ledBluePin = 10;
 
+void setup() {
+    pinMode(btnRedPin, INPUT_PULLUP);
+    pinMode(btnBluePin, INPUT_PULLUP);
+    pinMode(ledRedPin, OUTPUT);
+    pinMode(ledBluePin, OUTPUT);
 }
 
 void loop() {
-
+    int btnReadState = digitalRead(btnRedPin);
+    int btnBlueState = digitalRead(btnBlueState);
+    digitalWrite(ledRedPin, btnReadState);
+    digitalWrite(ledBluePin, btnBlueState);
 }
