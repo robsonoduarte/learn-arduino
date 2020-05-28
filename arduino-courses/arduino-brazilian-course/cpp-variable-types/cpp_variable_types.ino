f5928a6e9d5a1530ed2ecf446486ce4b48d7b60f
Robson Duarte <robson.o.d@gmail.com>
2020-05-28 12:56:36 -0300

    finished the skatch

--- arduino-courses/arduino-brazilian-course/cpp-variable-types/cpp_variable_types.ino
+++ arduino-courses/arduino-brazilian-course/cpp-variable-types/cpp_variable_types.ino
@@ -15,7 +15,38 @@ byte b = 255;
 //https://www.arduino.cc/reference/en/language/variables/data-types/char/
 char c = 'A';
 
-// stop https://www.arduino.cc/reference/en/language/variables/data-types/double/
+//https://www.arduino.cc/reference/en/language/variables/data-types/double/
+double d = 2.0;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/float/
+float f = 3.0;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/int/
+int i = 32767;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/long/
+long l = 2147483647;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/short/
+short s = -32768;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/size_t/
+size_t st = l;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/string/
+char cStr[8] = "Arduino";
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/stringobject/
+String str = "Arduino";
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/unsignedchar/
+unsigned char uc = 255;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/unsignedint/
+unsigned int ui = 65535;
+
+//https://www.arduino.cc/reference/en/language/variables/data-types/unsignedlong/
+unsigned long ul = 4294967295;
 
 void setup() {
     Serial.begin(9600);
@@ -36,8 +67,39 @@ void setup() {
     Serial.print("char variable => ");
     Serial.println(c);
 
+    Serial.print("double variable => ");
+    Serial.println(d);
+
+    Serial.print("float variable => ");
+    Serial.println(f);
+
+    Serial.print("int variable => ");
+    Serial.println(i);
+
+    Serial.print("long variable => ");
+    Serial.println(l);
+
+    Serial.print("short variable => ");
+    Serial.println(s);
+
+    Serial.print("size_t variable => ");
+    Serial.println(st);
+
+    Serial.print("String variable as char array => ");
+    Serial.println(cStr);
+
+    Serial.print("String variable => ");
+    Serial.println(str);
+
+    Serial.print("Unsigned char => ");
+    Serial.println(uc);
+
+    Serial.print("Unsigned int => ");
+    Serial.println(ui);
+
+    Serial.print("Unsigned long => ");
+    Serial.println(ul);
 }
 
 void loop() {
-
 }
