2cae9951e8ee6b7eca4c1c961d13c34558a341d8
Robson Duarte <robson.o.d@gmail.com>
2020-06-02 12:21:37 -0300

    add state ligth up

--- arduino-courses/arduino-brazilian-course/control-structures/control_structures.ino
+++ arduino-courses/arduino-brazilian-course/control-structures/control_structures.ino
@@ -1,16 +1,18 @@
 #include <Arduino.h>
 
 int btnBlinkPin = 2;
+int btnLightUpPin = 3;
 
 int ledRedPin = 8;
 int ledGreenPin = 9;
 int ledBluePin = 10;
 
-
 void blinkLed(int pin);
+void lightUp(int pin);
 
 void setup() {
     pinMode(btnBlinkPin, INPUT_PULLUP);
+    pinMode(btnLightUpPin, INPUT_PULLUP);
     pinMode(ledRedPin, OUTPUT);
     pinMode(ledGreenPin, OUTPUT);
     pinMode(ledBluePin,OUTPUT);
@@ -18,11 +20,20 @@ void setup() {
 
 void loop() {
     bool btnBlinkState = digitalRead(btnBlinkPin);
+    bool btnLightUpState = digitalRead(btnLightUpPin);
     if(!btnBlinkState){
         blinkLed(ledRedPin);
         blinkLed(ledGreenPin);
         blinkLed(ledBluePin);
+    }else if (!btnLightUpState){
+        lightUp(ledRedPin);
+        lightUp(ledGreenPin);
+        lightUp(ledBluePin);
     }
+}
+
+void lightUp(int pin) {
+    digitalWrite(pin, HIGH);
 }
 
 void blinkLed(int pin) {
