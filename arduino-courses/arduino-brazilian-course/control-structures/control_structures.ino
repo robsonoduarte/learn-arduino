c5acec1b8b8615e8db2f90c8140c8fc6c2ea0b0e
Robson Duarte <robson.o.d@gmail.com>
2020-06-04 12:19:24 -0300

    end sketch

--- arduino-courses/arduino-brazilian-course/control-structures/control_structures.ino
+++ arduino-courses/arduino-brazilian-course/control-structures/control_structures.ino
@@ -1,39 +1,59 @@
 #include <Arduino.h>
 
 int btnBlinkPin = 2;
-int btnLightUpPin = 3;
+int btnFadePin = 3;
 
 int ledRedPin = 8;
 int ledGreenPin = 9;
 int ledBluePin = 10;
 
+int brightness = 0;
+int fadeAmount = 5;
+
 void blinkLed(int pin);
-void lightUp(int pin);
+
+void fadeLed(int pin);
+
+void turnOffLed(int pin);
 
 void setup() {
     pinMode(btnBlinkPin, INPUT_PULLUP);
-    pinMode(btnLightUpPin, INPUT_PULLUP);
+    pinMode(btnFadePin, INPUT_PULLUP);
+
     pinMode(ledRedPin, OUTPUT);
     pinMode(ledGreenPin, OUTPUT);
-    pinMode(ledBluePin,OUTPUT);
+    pinMode(ledBluePin, OUTPUT);
 }
 
 void loop() {
-    bool btnBlinkState = digitalRead(btnBlinkPin);
-    bool btnLightUpState = digitalRead(btnLightUpPin);
-    if(!btnBlinkState){
+    bool btnBlinkState = !digitalRead(btnBlinkPin);
+    bool btnFadeState = !digitalRead(btnFadePin);
+    if (btnBlinkState) {
         blinkLed(ledRedPin);
         blinkLed(ledGreenPin);
         blinkLed(ledBluePin);
-    }else if (!btnLightUpState){
-        lightUp(ledRedPin);
-        lightUp(ledGreenPin);
-        lightUp(ledBluePin);
+    } else if (btnFadeState) {
+        fadeLed(ledRedPin);
+        fadeLed(ledGreenPin);
+        fadeLed(ledBluePin);
+    } else {
+        turnOffLed(ledRedPin);
+        turnOffLed(ledGreenPin);
+        turnOffLed(ledBluePin);
     }
 }
 
-void lightUp(int pin) {
-    digitalWrite(pin, HIGH);
+void turnOffLed(int pin) {
+    digitalWrite(pin, LOW);
+}
+
+void fadeLed(int pin) {
+    analogWrite(pin, brightness);
+    brightness = brightness + fadeAmount;
+    if (brightness <= 0 || brightness >= 255) {
+        fadeAmount = -fadeAmount;
+    }
+    delay(30);
 }
 
 void blinkLed(int pin) {
@@ -42,4 +62,3 @@ void blinkLed(int pin) {
     digitalWrite(pin, LOW);
     delay(500);
 }
-
