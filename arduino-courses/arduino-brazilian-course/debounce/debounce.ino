8225a267a4ef02b16140f40d38ec5f1b872d8d86
Robson Duarte <robson.o.d@gmail.com>
2020-06-18 14:48:11 -0300

    start sketch

--- arduino-courses/arduino-brazilian-course/debounce/debounce.ino
+++ arduino-courses/arduino-brazilian-course/debounce/debounce.ino
@@ -1,8 +1,68 @@
 #include <Arduino.h>
 
+int pinLedRead = 8;
+int pinLedGreen = 9;
+int pinLedBlue = 10;
+
+int pinBtnBlink = 3;
+int pinBtnFade = 4;
+
+void blinkLed(int pin);
+
+void fadeLed(int pin);
+
+void turnOffLed(int pin);
+
 void setup() {
+    pinMode(pinLedRead, OUTPUT);
+    pinMode(pinLedGreen, OUTPUT);
+    pinMode(pinLedBlue, OUTPUT);
+
+    pinMode(pinBtnBlink, INPUT_PULLUP);
+    pinMode(pinBtnFade, INPUT_PULLUP);
 }
 
+
 void loop() {
-    
+    blinkLed(pinLedRead);
+    blinkLed(pinLedGreen);
+    blinkLed(pinLedBlue);
+
+/*    fadeLed(pinLedRead);
+    fadeLed(pinLedGreen);
+    fadeLed(pinLedBlue);*/
+
+/*    turnOffLed(pinLedRead);
+    turnOffLed(pinLedGreen);
+    turnOffLed(pinLedBlue);*/
 }
+
+void blinkLed(int pin) {
+    int pinState = digitalRead(pin);
+    if(pinState){
+        digitalWrite(pin, LOW);
+    } else{
+        digitalWrite(pin, HIGH);
+    }
+    delay(1000);
+}
+
+void fadeLed(int pin) {
+/*    int fadeAmount = 10;
+    int brightness = 0;
+
+    while (brightness <= 1024){
+        brightness = brightness + fadeAmount;
+        analogWrite(pin, brightness);
+        delay(150);
+    }*/
+}
+
+void turnOffLed(int pin) {
+    digitalWrite(pin, LOW);
+    delay(1000);
+}
+
+
+
+
