d45d7e31e4e7d4565fd0ba54460a9f2bd70c4f08
Robson Duarte <robson.o.d@gmail.com>
2020-09-24 14:42:14 -0300

    :cry:

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -5,15 +5,17 @@
 #endif
 
 void setup(){
+	Serial.begin(9600);
 	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop(){
-//	digitalWrite(LED_BUILTIN, HIGH);
+	digitalWrite(LED_BUILTIN, HIGH);
 
 	delay(1000);
 
 	digitalWrite(LED_BUILTIN, LOW);
 
 	delay(1000);
+	Serial.println(BUILD_ENV_NAME);
 }
