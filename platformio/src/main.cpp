e17699aa64c9a6e2d6e62d9244932149477114f7
Robson Duarte <robson.o.d@gmail.com>
2020-10-30 16:57:48 -0300

    Inital Structure of Platformio Project Example (#76)

---
+++ platformio/src/main.cpp
@@ -0,0 +1,15 @@
+#include "Arduino.h"
+
+void setup(){
+	Serial.begin(9600);
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop(){
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(1000);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(1000);
+	Serial.print("Running on env => ");
+	Serial.println(BUILD_ENV_NAME);
+}
