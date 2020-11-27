e40e96da47a73b60aa278e86ca333300784e1694
Robson Duarte <robson.o.d@gmail.com>
2020-11-27 13:37:42 -0300

    :rocket:

--- esp8266/async-web-server/src/main.cpp
+++ esp8266/async-web-server/src/main.cpp
@@ -1,13 +1,21 @@
 #include "Arduino.h"
+#include "ESP8266WiFi.h"
 
 //https://randomnerdtutorials.com/esp8266-nodemcu-async-web-server-espasyncwebserver-library/
 
-const char* ssid = "**";
-const char* password = "**";
+const char* ssid = "*";
+const char* password = "*";
 
 
 void setup(){
+    Serial.begin(9600);
 
+    WiFi.begin(ssid,password);
+    while (WiFi.status() != WL_CONNECTED){
+        delay(100);
+        Serial.println("Connecting to Wifi...");
+    }
+    Serial.println(WiFi.localIP());
 }
 
 void loop(){
