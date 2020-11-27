bdf2926991037c47907bf589f3c4784c0b518027
Robson Duarte <robson.o.d@gmail.com>
2020-11-27 13:23:28 -0300

    :rocket:

--- esp8266/async-web-server/src/main.cpp
+++ esp8266/async-web-server/src/main.cpp
@@ -2,9 +2,12 @@
 
 //https://randomnerdtutorials.com/esp8266-nodemcu-async-web-server-espasyncwebserver-library/
 void setup(){
-
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop(){
-
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
