4a1ed90c6f4c5c8edcc86a04f9906c2583ab6276
Robson Duarte <robson.o.d@gmail.com>
2020-11-27 13:27:19 -0300

    :rocket:

--- esp8266/async-web-server/src/main.cpp
+++ esp8266/async-web-server/src/main.cpp
@@ -1,13 +1,14 @@
 #include "Arduino.h"
 
 //https://randomnerdtutorials.com/esp8266-nodemcu-async-web-server-espasyncwebserver-library/
+
+const char* ssid = "**";
+const char* password = "**";
+
+
 void setup(){
-    pinMode(LED_BUILTIN, OUTPUT);
+
 }
 
 void loop(){
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
 }
