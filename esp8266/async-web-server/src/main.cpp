d1db30f34b90ba9ccddd8167ac3b6e62a743c85c
Robson Duarte <robson.o.d@gmail.com>
2020-11-30 15:21:02 -0300

    :pray:

--- esp8266/async-web-server/src/main.cpp
+++ esp8266/async-web-server/src/main.cpp
@@ -6,17 +6,31 @@
 const char* ssid = "*";
 const char* password = "*";
 
+const int PIN_LED_RED = 5;
+const int PIN_LED_BLUE = 4;
+const int PIN_LED_YELLOW = 2;
+
 
 void setup(){
     Serial.begin(9600);
 
+    pinMode(PIN_LED_RED, OUTPUT);
+    pinMode(PIN_LED_BLUE, OUTPUT);
+    pinMode(PIN_LED_YELLOW, OUTPUT);
+
+    digitalWrite(PIN_LED_RED, LOW);
+    digitalWrite(PIN_LED_BLUE, LOW);
+    digitalWrite(PIN_LED_YELLOW, LOW);
+
     WiFi.begin(ssid,password);
     while (WiFi.status() != WL_CONNECTED){
-        delay(100);
+        delay(1000);
         Serial.println("Connecting to Wifi...");
     }
     Serial.println(WiFi.localIP());
 }
 
 void loop(){
+    Serial.println("running...");
+    delay(1000);
 }
